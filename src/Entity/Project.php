10ee93a17779427ecd4102c9b5b4327beff1b0ab
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2021-12-11 20:48:32 +0100

    Menu påbegyndt

---
+++ src/Entity/Project.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * This file is part of the Overlund package.
+ *
+ * (c) Michael Lindhardt Rasmussen <filicis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+
+
+namespace App\Entity;
+
+use Symfony\Bridge\Doctrine\IdGenerator\UlidGenerator;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+  /**
+   *  Project
+   *  -
+   *
+   *
+   * @ORM\Entity(repositoryClass=ProjectRepository::class)
+   **/
+
+
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="ulid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UlidGenerator::class)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}

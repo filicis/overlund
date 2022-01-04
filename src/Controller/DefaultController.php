7272656604f06f0c9039f8da24ff4a68c752b770
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-01-04 17:14:02 +0100

    Default revideret

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,18 +12,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use       Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use       Symfony\Component\HttpFoundation\Response;
+use       Symfony\Component\Routing\Annotation\Route;
+use       Doctrine\Persistence\ManagerRegistry;
+
+
+use       App\Entity\Project;
+use       App\Repository\ProjectRepository;
+
+
+  /**
+   *  DefaultController
+   *
+   *
+   **/
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'default')]
-    public function index(): Response
-    {
-        return $this->render('default/index.html.twig', [
-          'controller_name' => 'DefaultController',
-          'formTitle' => 'Default Controller',
-        ]);
+  #[Route('/', name: 'default')]
+  public function index(ManagerRegistry $doctrine): Response
+  {
+    $projects = $doctrine->getRepository(Project::class)->findAll();
+
+    if (! $projects) {
+      throw $this->createNotFoundException(
+      'No product found for id '
+      );
     }
+
+    return $this->render('default/index.html.twig', [
+    'controller_name' => 'DefaultController',
+    'formTitle' => 'Default Controller',
+    'trees' => $projects,
+    ]);
+  }
 }

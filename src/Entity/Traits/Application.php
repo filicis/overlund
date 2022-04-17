e49839730ef5aeec33654254f3655729073f6bff
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-04-17 20:54:27 +0200

    add MIT License

---
+++ src/Entity/Traits/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Overlund package.
+ *
+ * (c) Michael Lindhardt Rasmussen <filicis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ **/
+
+
+
+namespace App\Entity\Traits;
+
+
+
+  /**
+   *  trait Application
+   *
+   **/
+
+
+trait Application
+{
+  
+  public const SOUR               = "Overlund - Web-based Genealogy Application";
+  public const SOUR_VERS          = "0.1.0 alpha";
+  public const SOUR_NAME          = "Michael Lindhardt Rasmussen";
+  public const SOUR_CORP          = "Filicis Software";  
+  public const SOUR_EMAIL         = "michael@filicis.dk";
+  public const SOUR_WWW           = "https://github.com/filicis/overlund"; 
+}

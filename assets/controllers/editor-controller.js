274f33649c591eb5ced6b2b59bcdb14d1ba1abcf
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-05-16 23:04:30 +0200

    stimulus-controller editor påbegyndt

---
+++ assets/controllers/editor-controller.js
@@ -0,0 +1,54 @@
+// assets/controllers/editor-controller.js
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static values= {
+    project: String,
+    indi: String,
+    fam: String
+  }
+  static targets= ['indiView', 'famView']
+
+
+  // indiValueChanged()
+  // - opdaterer 
+  //
+  indiValueChanged() 
+  {
+    //fetch(this.urlValue).then(/* … */)
+  }
+
+
+  // famValueChanged()
+  // - opdaterer
+  //
+  famValueChanged() 
+  {
+    //fetch(this.urlValue).then(/* … */)
+  }
+
+
+  // updateSex()
+  //
+  //
+  updateSex(event) 
+  {
+     document.getElementById("birger").classList.remove('bg-primary', 'bg-secondary', 'bg-danger', 'bg-alarm');
+       switch (document.getElementById("idXXXXXX7").value)
+       {
+         case 'M':
+           document.getElementById("birger").classList.add("bg-primary");
+           // document.getElementById("birger").style.backgroundColor= tomato;
+           break;
+         case 'F':
+           document.getElementById("birger").classList.add("bg-danger");
+           break;
+         case 'U':
+         case 'N':
+           document.getElementById("birger").classList.add("bg-secondary");
+           break;
+         default:
+           document.getElementById("birger").classList.add("bg-alarm");
+       }
+  }
+}

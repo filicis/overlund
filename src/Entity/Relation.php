e80a5869a88f906280c7cbbcf1a260cee6182471
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-07-12 22:09:56 +0200

    Composer Update: Doctrine stuff

--- src/Entity/Relation.php
+++ src/Entity/Relation.php
@@ -1,96 +1,121 @@
 <?php
 
+/**
+ * This file is part of the Overlund package.
+ *
+ * @author Michael Lindhardt Rasmussen <filicis@gmail.com>
+ * @copyright 2000-2022 Filicis Software
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+
 namespace App\Entity;
 
 use App\Repository\RelationRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ *  class Relation
+ *
+ *  Implementerer Gedcom FAMC, FAMS, HUSB, WIFE, CHIL
+ */
+
 #[ORM\Entity(repositoryClass: RelationRepository::class)]
 class Relation
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private $id;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private $id;
 
-    #[ORM\Column(type: 'string', length: 4)]
-    private $type;
 
-    #[ORM\Column(type: 'string', length: 16, nullable: true)]
-    private $pedi;
+  /**
+   *
+   *
+   */
 
-    #[ORM\Column(type: 'string', length: 16, nullable: true)]
-    private $stat;
+  #[ORM\Column(type: 'string', length: 4)]
+  private $type;
 
-    #[ORM\ManyToOne(targetEntity: Individual::class, inversedBy: 'relations')]
-    private $individual;
+  #[ORM\Column(type: 'string', length: 16, nullable: true)]
+  private $pedi;
 
-    #[ORM\ManyToOne(targetEntity: Family::class, inversedBy: 'relations')]
-    #[ORM\JoinColumn(nullable: false)]
-    private $family;
+  #[ORM\Column(type: 'string', length: 16, nullable: true)]
+  private $stat;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  #[ORM\ManyToOne(targetEntity: Individual::class, inversedBy: 'relations')]
+  private $individual;
 
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
+  #[ORM\ManyToOne(targetEntity: Family::class, inversedBy: 'relations')]
+  #[ORM\JoinColumn(nullable: false)]
+  private $family;
 
-    public function setType(string $type): self
-    {
-        $this->type = $type;
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-        return $this;
-    }
+  public function getType(): ?string
+  {
+    return $this->type;
+  }
 
-    public function getPedi(): ?string
-    {
-        return $this->pedi;
-    }
+  public function setType(string $type): self
+  {
+    $this->type = $type;
 
-    public function setPedi(?string $pedi): self
-    {
-        $this->pedi = $pedi;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getPedi(): ?string
+  {
+    return $this->pedi;
+  }
 
-    public function getStat(): ?string
-    {
-        return $this->stat;
-    }
+  public function setPedi(?string $pedi): self
+  {
+    $this->pedi = $pedi;
 
-    public function setStat(?string $stat): self
-    {
-        $this->stat = $stat;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getStat(): ?string
+  {
+    return $this->stat;
+  }
 
-    public function getIndividual(): ?Individual
-    {
-        return $this->individual;
-    }
+  public function setStat(?string $stat): self
+  {
+    $this->stat = $stat;
 
-    public function setIndividual(?Individual $individual): self
-    {
-        $this->individual = $individual;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getIndividual(): ?Individual
+  {
+    return $this->individual;
+  }
 
-    public function getFamily(): ?Family
-    {
-        return $this->family;
-    }
+  public function setIndividual(?Individual $individual): self
+  {
+    $this->individual = $individual;
 
-    public function setFamily(?Family $family): self
-    {
-        $this->family = $family;
+    return $this;
+  }
 
-        return $this;
-    }
+  public function getFamily(): ?Family
+  {
+    return $this->family;
+  }
+
+  public function setFamily(?Family $family): self
+  {
+    $this->family = $family;
+
+    return $this;
+  }
 }

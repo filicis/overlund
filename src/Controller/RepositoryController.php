6b9f782a403396306f59a65e17251ea470bf535c
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-12-09 14:23:51 +0100

    AddressType rettelser

--- src/Controller/RepositoryController.php
+++ src/Controller/RepositoryController.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * This file is part of the Overlund package.
- *
- * @author Michael Lindhardt Rasmussen <filicis@gmail.com>
- * @copyright 2000-2022 Filicis Software
- * @license MIT
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+* This file is part of the Overlund package.
+*
+* @author Michael Lindhardt Rasmussen <filicis@gmail.com>
+* @copyright 2000-2022 Filicis Software
+* @license MIT
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace App\Controller;
 
@@ -20,80 +20,75 @@ use       Symfony\Component\Routing\Annotation\Route;
 
 use       Symfony\Component\Form\Extension\Core\Type\TextType;
 
-
 use       Doctrine\Persistence\ManagerRegistry;
-
 
 use       App\Entity\Project;
 use       App\Entity\RepositoryRecord;
+use       App\Entity\AddressStructure;
+use       App\Form\AddressType;
 
-class RepositoryController extends AbstractController
-{
-    #[Route('/repository', name: 'app_repository')]
-    public function index(): Response
-    {
-        return $this->render('repository/index.html.twig', [
+class RepositoryController extends AbstractController {
+    #[ Route( '/repository', name: 'app_repository' ) ]
+
+    public function index(): Response {
+        return $this->render( 'repository/index.html.twig', [
             'controller_name' => 'RepositoryController',
-        ]);
+        ] );
     }
 
-  /**
-   * 
-   */
+    /**
+    *
+    */
 
-  #[Route('/newrepository/{url}', name: 'newrepository')]
-  public function new(Request $request, Project $project, ManagerRegistry $doctrine): Response
-  {
-    $record= new RepositoryRecord();
+    #[ Route( '/newrepository/{url}', name: 'newrepository' ) ]
 
-    $form = $this->createFormBuilder($record)
-      ->add('name', TextType::class)
-      ->add('addr', TextType::class)
-      ->add('adr1', TextType::class)
-      ->add('adr2', TextType::class)
-      ->add('adr3', TextType::class)
+    public function new( Request $request, Project $project, ManagerRegistry $doctrine ): Response {
+        $record = new RepositoryRecord();
 
-      ->add('post', TextType::class)
-      ->add('city', TextType::class)
-      ->add('stae', TextType::class)
-      ->add('ctry', TextType::class)
-      ->add('phon', TextType::class)
-      ->add('email', TextType::class)
-      ->add('fax', TextType::class)
-      ->add('www', TextType::class)
-      ->getForm();
+        /*
+        $form = $this->createFormBuilder( $record )
+        ->add( 'name', TextType::class )
+        ->add( 'addr', TextType::class )
+        ->add( 'adr1', TextType::class )
+        ->add( 'adr2', TextType::class )
+        ->add( 'adr3', TextType::class )
 
+        ->add( 'post', TextType::class )
+        ->add( 'city', TextType::class )
+        ->add( 'stae', TextType::class )
+        ->add( 'ctry', TextType::class )
+        ->add( 'phon', TextType::class )
+        ->add( 'email', TextType::class )
+        ->add( 'fax', TextType::class )
+        ->add( 'www', TextType::class )
+        ->getForm();
+        */
 
-    $form->handleRequest($request);
+        $form = $this->createForm( AddressType::class, $record );
 
-		if ($form->isSubmitted() && $form->isValid())
-		{
-		  try
-		  {
-        $entityManager= $doctrine->getManager();
+        $form->handleRequest( $request );
 
-              // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($record);
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            try {
+                $entityManager = $doctrine->getManager();
 
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
+                // tell Doctrine you want to ( eventually ) save the Product ( no queries yet )
+                $entityManager->persist( $record );
 
-			  //return $this->redirectToRoute('/da');
-      }
-      catch(\Exception $e)
-      {
-      }
-		}
+                // actually executes the queries ( i.e. the INSERT query )
+                $entityManager->flush();
 
+                //return $this->redirectToRoute( '/da' );
+            } catch( \Exception $e ) {
+            }
+        }
 
+        return $this->renderForm( 'editor/modal/repository1.html.twig', [
+            'form' => $form,
+            'project' => $project,
+            'controller_name' => 'ProjectController',
+        ] );
 
-    return $this->renderForm('editor/modal/repository1.html.twig', [
-    'form' => $form,
-    'project' => $project,
-    'controller_name' => 'ProjectController',
-    ]);
-
-
-  }
+    }
 
 }
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -14,17 +14,17 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\EmailType;
-use Symfony\Component\Form\TelType;
-use Symfony\Component\Form\TextareaType;
-use Symfony\Component\Form\TextType;
-use Symfony\Component\Form\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextAreaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\AddressStructure;
 
-class AddressType extends AbstractType  {
-    public function buildForm( FormBuilderInterface $builder, array $options ): void  {
+class AddressType extends AbstractType {
+    public function buildForm( FormBuilderInterface $builder, array $options ): void {
         $builder
         ->add( 'addr', TextType::class )
         ->add( 'adr1', TextType::class )
@@ -42,7 +42,7 @@ class AddressType extends AbstractType  {
         ;
     }
 
-    public function configureOptions( OptionsResolver $resolver ): void  {
+    public function configureOptions( OptionsResolver $resolver ): void {
         $resolver->setDefaults(
             [
                 'data_class' => AddressStructure::class,

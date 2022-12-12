3eb164e9d6418f671b72c2fd5bec8689eca00547
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-12-12 22:26:07 +0100

    Stimulus controller oprydning

--- assets/controllers/reload-content_controller.js
+++ assets/controllers/reload-content_controller.js
@@ -1,5 +1,21 @@
 import { Controller } from '@hotwired/stimulus';
 
+/**
+ * This file is part of the Overlund package.
+ *
+ * @author Michael Lindhardt Rasmussen <filicis@gmail.com>
+ * @copyright 2000-2022 Filicis Software
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// assets/controllers/reload-content_controller.js
+
+
+
+
 /*
 * The following line makes this controller "lazy": it won't be downloaded until needed
 * See https://github.com/symfony/stimulus-bridge#lazy-controllers
@@ -25,6 +41,7 @@ export default class extends Controller {
       credentials: 'include',
     };
 
+    console.log('Parameters: ', event.params);
     console.time('render1');
     await fetch(this.urlValue, myInit)
       .finally(() => {

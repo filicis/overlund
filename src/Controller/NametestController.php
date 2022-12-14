00483db40a2ea63ae653f02be37866bcec0ac370
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2022-12-14 01:06:38 +0100

    NameStructure Form smårettelser

--- src/Controller/NametestController.php
+++ src/Controller/NametestController.php
@@ -11,27 +11,25 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
-
 use App\Entity\NameStructure;
 use App\Form\NamePiecesType;
 
-class NametestController extends AbstractController
-{
-  #[Route('/nametest', name: 'app_nametest')]
-  public function index(): Response
-  {
-    $name= new NameStructure();
+class NametestController extends AbstractController {
+    #[ Route( '/nametest', name: 'app_nametest' ) ]
 
-    $form = $this->createFormBuilder($name)
-                 ->add('personalName', TextType::class, ['label' => 'Personal Name'])
-      ->add('nameType', TextType::class,['label' => 'Nametype'])         
-      ->add('save', SubmitType::class, ['label' => 'Submit'])
-      ->add('namePieces', NamePiecesType::class)
-    
-      ->getForm();
-    return $this->render('nametest/index.html.twig', [
-      'controller_name' => 'NametestController',
-      'form' => $form,
-        ]);
-  }
+    public function index(): Response {
+        $name = new NameStructure();
+
+        $form = $this->createFormBuilder( $name )
+        ->add( 'personalName', TextType::class, [ 'label' => 'Personal Name' ] )
+        ->add( 'nameType', TextType::class, [ 'label' => 'Nametype' ] )
+        ->add( 'save', SubmitType::class, [ 'label' => 'Submit' ] )
+        ->add( 'namePieces', NamePiecesType::class )
+
+        ->getForm();
+        return $this->render( 'nametest/index.html.twig', [
+            'controller_name' => 'NametestController',
+            'form' => $form,
+        ] );
+    }
 }
